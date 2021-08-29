63817dee5ba2c7bff0896dd13805421b8949a14a
Honoré Rasamoelina <10617191+Honomatopic@users.noreply.github.com>
2021-08-29 22:54:50 +0200

    Refactoring projet avec PostGreSQL, GNU/Linux (Ubuntu) et Notepadqq
    Refactoring projet avec PostGreSQL, GNU/Linux (Ubuntu) et Notepadqq

--- deconnexion.traitement.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Script de déconnexion de la session de l'établissement authentifié
-if (isset($_POST["deconnecter"])) {
-    session_destroy();
-    header('location:index.php');
-    exit();
-}
