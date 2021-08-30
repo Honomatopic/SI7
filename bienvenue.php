6f2b513e9d2ae446a9257ded19b93476228c745f
Honoré Rasamoelina <10617191+Honomatopic@users.noreply.github.com>
2021-08-30 22:56:52 +0200

    Refactoring projet avec PostGreSQL, GNU/Linux (Ubuntu) et Notepadqq
    Refactoring projet avec PostGreSQL, GNU/Linux (Ubuntu) et Notepadqq

--- bienvenue.php
+++ bienvenue.php
@@ -8,7 +8,6 @@ require_once ('_entetemenu.inc.php');
 		<i>Clique en bas sur la bannière pour jouer avec Calculnette : </i>
 		<br>
 		<a href="calculette.php"><img src="images/mangerbouger.jpg" /> </a>
-
 </div>
 </body>
 </html>
