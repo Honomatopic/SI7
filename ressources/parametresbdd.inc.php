d2efb7cf2067059d593c679ec19e6c278869ca8a
Honoré Rasamoelina <10617191+Honomatopic@users.noreply.github.com>
2021-08-31 21:00:13 +0200

    Refactoring projet avec GNU/Linux (Ubuntu), PostGreSQL et Notepadqq
    Refactoring projet avec GNU/Linux (Ubuntu), PostGreSQL et Notepadqq

--- ressources/parametresbdd.inc.php
+++ ressources/parametresbdd.inc.php
@@ -1,9 +1,7 @@
 <?php
-
 /*
  * Identifiants de connexion à la base de données calcul
  */
-
 define('DSNBDD', 'mysql:host=localhost;dbname=calcul;charset=utf8');
 define('UTILISATEURBDD', 'root');
 define('MOTPASSEBDD', '');
